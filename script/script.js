823a7a79802626debd734616d4c5eaf32d4fe6bd
eduardosdl <edu.silva.e08@gmail.com>
2022-07-21 15:37:30 -0300

    structuring html and bootstrap

--- script/script.js
+++ script/script.js
@@ -1,22 +1,3 @@
-// const URL = 'https://api.hgbrasil.com/weather?key=4cda53da&city_name=Paulista,PE'
-// const city = 'Recife,PE'
-// const URLCity = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=8f77d0065bcd16d453b9e3093004900c#`;
-// const URLclima = 'https://api.openweathermap.org/data/2.5/onecall?lat=-7.950487&lon=-34.874409&exclude=hourly,minutely&units=metric&lang=pt_br&appid=8f77d0065bcd16d453b9e3093004900c#';
-
-
-/* fetch(URL).then((res) => {
-    return res.json()
-}).then((teste) => console.log(teste)).catch((err) => {
-    console.log(err);
-})
-
-fetch(URLcity).then((res) => {
-    return res.json()
-}).then((teste) => console.log(teste)).catch((err) => {
-    console.log(err);
-}) */
-
-
 const teste = async function() {
     const city = 'Recife'
     const URLCity = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=8f77d0065bcd16d453b9e3093004900c#`;
@@ -36,7 +17,7 @@ const teste = async function() {
     console.log(dataClima);
 
 
-    let dataUnix = 1658412000
+    let dataUnix = dataClima.current.dt
     let data = new Date(dataUnix*1000);
     console.log(data)
 
