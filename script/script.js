72d54a9fa14155a8f52029c1a3dead5efbd0b35c
eduardosdl <edu.silva.e08@gmail.com>
2022-07-22 09:30:32 -0300

    layout ready

--- script/script.js
+++ script/script.js
@@ -7,7 +7,7 @@ const teste = async function() {
     const latCity = dataCity[0].lat
     const lonCity = dataCity[0].lon
 
-    const URLClima = `https://api.openweathermap.org/data/2.5/onecall?lat=${latCity}&lon=${lonCity}&exclude=hourly,minutely&units=metric&lang=pt_br&appid=8f77d0065bcd16d453b9e3093004900c#`;
+    const URLClima = `https://api.openweathermap.org/data/2.5/onecall?lat=${latCity}&lon=${lonCity}&exclude=minutely&units=metric&lang=pt_br&appid=8f77d0065bcd16d453b9e3093004900c#`;
 
     const clima = await fetch(URLClima)
     const dataClima = await clima.json();
