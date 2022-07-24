357adac3820b2fc1ac33e1f8e4b689887603acb1
eduardosdl <edu.silva.e08@gmail.com>
2022-07-24 00:40:27 -0300

    previous daily completed

--- script/script.js
+++ script/script.js
@@ -20,11 +20,14 @@ function getCity () {
     } else {
         input.classList.remove('danger');
         fetch(urlCity).then(res => res.json()).then((dataCity) => {
-            document.querySelector('#cityName').innerHTML = `${dataCity[0].name}, ${dataCity[0].country}`
+            document.querySelector('#cityName').innerHTML = `${dataCity[0].name}, ${dataCity[0].country}`;
             getClimate(dataCity[0].lat, dataCity[0].lon);
-        }).catch(err => alert("Houve um erro, tente novamente"));
+        }).catch((err) => {
+            alert("Houve um erro, tente novamente");
+            console.log(err);
+        });
 
-        input.value = ""
+        input.value = "";
     }
 }
 
@@ -34,13 +37,16 @@ function getClimate(lat, lon) {
     fetch(urlClimate).then(res => res.json()).then((dataClimate) => {
         console.log(dataClimate);
         climateCurrent(dataClimate);
-        climateHour(dataClimate);
+        climateHour(dataClimate.hourly);
 
-    }).catch(err => alert("Houve um erro, tente novamente"));
+    }).catch((err) => {
+        alert("Houve um erro, tente novamente");
+        console.log(err);
+    });
 }
 
 function climateCurrent (data) {
-    const date = new Date(data.current.dt*1000)
+    const date = new Date(data.current.dt*1000);
     const dateFormted = (new Intl.DateTimeFormat('pt-BR').format(date));
     const hour = `${date.getHours()}:${date.getMinutes()}`;
 
@@ -53,4 +59,42 @@ function climateCurrent (data) {
     
     document.querySelector('#maxCurrent').innerHTML = `${data.daily[0].temp.max.toFixed(1)}&deg;C`;
     document.querySelector('#minCurrent').innerHTML = `${data.daily[0].temp.min.toFixed(1)}&deg;C`;
+}
+
+function climateHour (data) {
+    const list = document.querySelector('#previousHour');
+
+    for (const hour of data) {
+        const date = new Date(hour.dt*1000);
+
+        const cardHour = createCard({
+            hour: `${date.getHours()}:${date.getMinutes().toLocaleString()}`,
+            img: `../img/${hour.weather[0].icon}.png`,
+            temp: `${hour.temp.toFixed(1)}&deg;C`,
+        });
+
+        list.append(cardHour);
+        
+    }
+}
+
+function createCard(content) {
+    const card = document.createElement('li');
+    const hour = document.createElement('h5');
+    const img = document.createElement('img');
+    const temp = document.createElement('h5');
+
+    card.setAttribute('class', 'mx-2 px-2 rounded d-flex lign-items-center flex-column');
+    hour.classList.add('fs-5');
+    temp.classList.add('fs-5');
+
+    img.setAttribute('src', content.img);
+    hour.innerHTML = content.hour;
+    temp.innerHTML = content.temp;
+
+    card.append(hour);
+    card.append(img);
+    card.append(temp);
+
+    return card;
 }
